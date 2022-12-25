cd307bf49115c7bb853493fa78f3d8ba61d9f8ec
daljeetkumar141 <96333976+daljeetkumar141@users.noreply.github.com>
2022-12-25 21:02:19 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 10) {
-		header.style.backgroundColor = '#060606';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
